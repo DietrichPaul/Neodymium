dea3b800ab77ae05bd3cd751af336f549629d725
cach30verfl0w <cach30verfl0w@gmail.com>
2022-05-13 11:04:12 +0200

    Replaced exception with fallback information message

--- src/main/java/de/cacheoverflow/jupiterclient/injections/mixins/client/MinecraftClientMixin.java
+++ src/main/java/de/cacheoverflow/jupiterclient/injections/mixins/client/MinecraftClientMixin.java
@@ -1,17 +1,24 @@
 package de.cacheoverflow.jupiterclient.injections.mixins.client;
 
+import com.mojang.authlib.minecraft.UserApiService;
+import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import de.cacheoverflow.jupiterclient.JupiterClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.RunArgs;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
 
+    @Shadow @Final private static Logger LOGGER;
     @Unique private JupiterClient jupiterClient;
 
     @Inject(
@@ -44,6 +51,20 @@ public class MinecraftClientMixin {
     )
     public void injectStopOnHead(CallbackInfo callback) {
         this.jupiterClient.stop();
+    }
+
+    @Inject(
+            method = "createUserApiService",
+            at = @At(
+                    target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V",
+                    shift = At.Shift.BEFORE,
+                    value = "INVOKE"
+            ),
+            cancellable = true
+    )
+    public void injectCreateUserApiServiceBeforeException(YggdrasilAuthenticationService authService, RunArgs runArgs, CallbackInfoReturnable<UserApiService> callback) {
+        LOGGER.warn("Can't authenticate account. Switch to offline authentication...");
+        callback.setReturnValue(UserApiService.OFFLINE);
     }
 
 }
